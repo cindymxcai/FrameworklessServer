5f05d2516b65248df9a84a05032a8de39450d533
Cindy Cai <cindymxcai@gmail.com>
2020-09-11 12:01:54 +1200

    able to delete user

--- FrameworklessServer/Users.cs
+++ FrameworklessServer/Users.cs
@@ -1,19 +1,33 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace FrameworklessServer
 {
     public class Users
     {
         public List<User> AllUsers { get; }
+        private readonly User _owner;
+
 
         public Users()
         {
-            AllUsers = new List<User>{new User("cindy")};
+            _owner = new User("Cindy");
+            AllUsers = new List<User>{_owner};
         }
 
         public void Add(User user)
         {
             AllUsers.Add(user);
+        }
+
+        public void Delete(string name)
+        {
+            if (name == _owner.Name)
+                throw new ArgumentException("Cindy owns this world! Cannot delete");
+            var userToDelete = AllUsers.First(p => p.Name == name);
+            AllUsers.Remove(userToDelete);
+
         }
     }
 }
--- FrameworklessServerTests/UserTests.cs
+++ FrameworklessServerTests/UserTests.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using FrameworklessServer;
+using Newtonsoft.Json;
 using Xunit;
 
 namespace FrameworklessServerTests
@@ -13,28 +15,53 @@ namespace FrameworklessServerTests
             var users = new Users();
             var result = users.AllUsers;
 
-            var expected = new List<User>{new User("cindy")};
+            var expected = new List<User>{new User("Cindy")};
            
             Assert.Equal(expected[0].Name, result[0].Name);
         }
         
         
         [Fact]
-        public void GivenNewPersonShouldAddToUsers()
+        public void GivenUserNameShouldAddToUsers()
         {
             var users = new Users();
 
-            users.Add(new User("bob"));
+            users.Add(new User("Bob"));
             var result = users.AllUsers;
             var expected = new List<User>
             {
-                new User("cindy"), new User("bob")
+                new User("Cindy"), new User("Bob")
             };
 
             Assert.Equal(2, result.Count);
             Assert.Equal(expected[0].Name, result[0].Name);
             Assert.Equal(expected[1].Name, result[1].Name);
+        }
+        
+        [Fact]
+        public void GivenUserNameShouldDeleteFromUsers()
+        {
+            var users = new Users();
+            users.Add(new User("Mary"));
+            users.Add(new User("Bob"));
 
+            Assert.Equal(3, users.AllUsers.Count);
+            users.Delete("Bob");
+            var expected = new List<User>
+            {
+                new User("Cindy"), new User("Mary")
+            };
+
+            Assert.Equal(2, users.AllUsers.Count);
+            Assert.Equal(expected[0].Name, users.AllUsers[0].Name);
+        }
+        
+                
+        [Fact]
+        public void GivenDeletingNameOfWorldOwnerShouldThrowException()
+        {
+            var users = new Users();
+            Assert.Throws<ArgumentException>(() => users.Delete("Cindy"));
         }
     }
 }
