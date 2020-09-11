ed32390fc21cf7b446050273f9087de3a4443bbb
Cindy Cai <cindymxcai@gmail.com>
2020-09-11 15:09:46 +1200

    refactoring existing methods to work with holding user list in json

--- FrameworklessServer/Users.cs
+++ FrameworklessServer/Users.cs
@@ -1,33 +1,56 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace FrameworklessServer
 {
     public class Users
     {
-        public List<User> AllUsers { get; }
         private readonly User _owner;
-
 
         public Users()
         {
-            _owner = new User("Cindy");
-            AllUsers = new List<User>{_owner};
+            _owner = GetAllUsers()[0];
         }
 
         public void Add(User user)
         {
-            AllUsers.Add(user);
+            var allUsers = GetAllUsers();
+            allUsers.Add(user);
+            CreateNewJArray(allUsers);
         }
-
+        
         public void Delete(string name)
         {
             if (name == _owner.Name)
                 throw new ArgumentException("Cindy owns this world! Cannot delete");
-            var userToDelete = AllUsers.First(p => p.Name == name);
-            AllUsers.Remove(userToDelete);
 
+            var allUsers = GetAllUsers();
+            allUsers.Remove(allUsers.First(u => u.Name == name));
+            CreateNewJArray(allUsers);
+        }
+
+        public void CreateNewJArray(List<User> allUsers)
+        {
+            var newUsers = allUsers.Select(t => new JObject(new JProperty("Name", t.Name)));
+            var newJson = new JArray(newUsers);
+            var streamWriter = new StreamWriter(Path.Combine(Directory.GetCurrentDirectory(), "UsersList.json"));
+            streamWriter.WriteLine(newJson);
+            
+            streamWriter.Close();
+        }
+
+        public List<User> GetAllUsers()
+        {
+            var streamReader = new StreamReader(Path.Combine(Directory.GetCurrentDirectory(), "UsersList.json"));
+            var jsonString = streamReader.ReadToEnd();
+           
+            streamReader.Close();
+
+            return JsonConvert.DeserializeObject<List<User>>(jsonString);
         }
     }
 }
--- FrameworklessServerTests/UserTests.cs
+++ FrameworklessServerTests/UserTests.cs
@@ -9,15 +9,25 @@ namespace FrameworklessServerTests
     public class UserTests
     {
 
+        private void ResetList()
+        {
+             var users = new Users();
+             var  allUsers = users.GetAllUsers();
+             allUsers.RemoveAll(user => user.Name != "Cindy");
+             users.CreateNewJArray(allUsers);
+        }
+        
+        
         [Fact]
         public void GivenListOfUsersShouldHaveCindyByDefault()
         {
             var users = new Users();
-            var result = users.AllUsers;
+            var result = users.GetAllUsers();
 
             var expected = new List<User>{new User("Cindy")};
            
             Assert.Equal(expected[0].Name, result[0].Name);
+            ResetList();
         }
         
         
@@ -27,7 +37,7 @@ namespace FrameworklessServerTests
             var users = new Users();
 
             users.Add(new User("Bob"));
-            var result = users.AllUsers;
+            var result = users.GetAllUsers();
             var expected = new List<User>
             {
                 new User("Cindy"), new User("Bob")
@@ -36,6 +46,8 @@ namespace FrameworklessServerTests
             Assert.Equal(2, result.Count);
             Assert.Equal(expected[0].Name, result[0].Name);
             Assert.Equal(expected[1].Name, result[1].Name);
+            ResetList();
+
         }
         
         [Fact]
@@ -45,23 +57,25 @@ namespace FrameworklessServerTests
             users.Add(new User("Mary"));
             users.Add(new User("Bob"));
 
-            Assert.Equal(3, users.AllUsers.Count);
+            Assert.Equal(3, users.GetAllUsers().Count);
             users.Delete("Bob");
             var expected = new List<User>
             {
                 new User("Cindy"), new User("Mary")
             };
 
-            Assert.Equal(2, users.AllUsers.Count);
-            Assert.Equal(expected[0].Name, users.AllUsers[0].Name);
+            Assert.Equal(2, users.GetAllUsers().Count);
+            Assert.Equal(expected[0].Name, users.GetAllUsers()[0].Name);
+            ResetList();
         }
         
-                
         [Fact]
         public void GivenDeletingNameOfWorldOwnerShouldThrowException()
         {
             var users = new Users();
             Assert.Throws<ArgumentException>(() => users.Delete("Cindy"));
+            ResetList();
+
         }
     }
 }
