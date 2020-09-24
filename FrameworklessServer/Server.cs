dfd104cb5c1d3760dae8d041c0e44e32ddad4695
Cindy Cai <cindymxcai@gmail.com>
2020-09-24 18:47:31 +1200

    router merge and fixed response duplication

--- FrameworklessServer/Server.cs
+++ FrameworklessServer/Server.cs
@@ -23,15 +23,13 @@ namespace FrameworklessServer
                 var context = _listener.GetContext();
                 var segmentedUrl = context.Request.Url.Segments.Aggregate("", (current, segment) => current + segment);
                 var request = new Request {Method = context.Request.HttpMethod, Path = segmentedUrl, Body = context};
-                var controller = _router.GetController(segmentedUrl);
+                var controller = _router.GetController(request.Path);
                 
                 var response = controller.HandleRequest(request).Body;
                 
                 var buffer = System.Text.Encoding.UTF8.GetBytes(response);
                 context.Response.ContentLength64 = buffer.Length;
                 context.Response.OutputStream.Write(buffer, 0, buffer.Length);
-                context.Response.OutputStream.Write(buffer, 0, buffer.Length);
-
             }
         }
     }
